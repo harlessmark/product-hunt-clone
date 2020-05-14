93f5f946fecac266238de7d886c85b5cbe7dfc51
Mark Harless <mark@Marks-MBP.attlocal.net>
2020-05-14 16:08:12 -0400

    Products model

--- producthunt-project/product/models.py
+++ producthunt-project/product/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+# import Django's User model
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=50)
+    url = models.URLField(max_length=200)
+    pub_date = models.DateTimeField()
+    votes_total = models.IntegerField(default=1)
+    image = models.ImageField(upload_to='images/')
+    icon = models.ImageField(upload_to='images/')
+    body = models.TextField()
+    # CASCADE means when user deletes their account, all things connected with it get deleted too
+    hunter = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
